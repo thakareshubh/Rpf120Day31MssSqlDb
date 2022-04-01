f6522a786be4f8839b4bae7d4873d166b93dee88
thakareshubh <shubhamthakare032@gmail.com>
2022-04-01 15:36:19 +0530

    Added code to select table data

--- Uc1CreatDataBase.sql
+++ Uc1CreatDataBase.sql
@@ -7,7 +7,15 @@ Name varchar(50) Not Null,
 Salary varchar(50) Not Null,
 StartDate datetime default GetDate(),
 )
+
 insert into employee_payroll
 values(4,'kuresh',1500,GETDATE()),
 (5,'Proohan',500,GETDATE());
+
+select * from employee_payroll;
+
+select  Salary from employee_payroll            #selecting salary of duresh
+where Name='Duresh';
+
 select * from employee_payroll
+where StartDate BETWEEN CAST('2022-04-01'  as  DATE) AND DATE(NOW());
