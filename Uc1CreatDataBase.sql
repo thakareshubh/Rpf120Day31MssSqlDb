f4a49d2883c88050544755b0dcdb966958ec4662
thakareshubh <shubhamthakare032@gmail.com>
2022-04-01 08:15:00 +0530

    creat table

--- Uc1CreatDataBase.sql
+++ Uc1CreatDataBase.sql
@@ -1,2 +1,10 @@
 create database payroll_service;
 use payroll_service;
+create table employye_payroll
+(
+Id int primary key,
+Name varchar(50),
+Salary varchar(50) Not Null,
+StartDate datetime default GetDate(),
+)
+EXEC sp_rename 'employye_payroll', 'employee_payroll';
