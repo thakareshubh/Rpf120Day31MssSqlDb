151e7856780d36e79f66a1981f185e3593f64400
thakareshubh <shubhamthakare032@gmail.com>
2022-04-02 08:05:11 +0530

    Added arithmatic data function operater

--- Uc1CreatDataBase.sql
+++ Uc1CreatDataBase.sql
@@ -4,7 +4,7 @@ create table employee_payroll
 (
 Id int Identity primary key,
 Name varchar(50) Not Null,
-Salary varchar(50) Not Null,
+Salary int (50) Not Null,
 StartDate datetime default GetDate(),
 )
 
@@ -14,8 +14,39 @@ values(4,'kuresh',1500,GETDATE()),
 
 select * from employee_payroll;
 
-select  Salary from employee_payroll            #selecting salary of duresh
+select  Salary from employee_payroll           
 where Name='Duresh';
 
-select * from employee_payroll
-where StartDate BETWEEN CAST('2022-04-01'  as  DATE) AND DATE(NOW());
+
+ALTER TABLE employee_payroll 
+ADD Gender CHAR(1) ;
+
+update employee_payroll 
+set Gender ='F' where Name='Proohan'; 
+
+update employee_payroll
+ set Gender ='M' where Name='Duresh';
+
+update employee_payroll 
+set Gender ='F' where Name='Kuresh';
+
+update employee_payroll 
+set Gender ='M' where Name='Shubham';
+
+ALTER TABLE employee_payroll
+ALTER COLUMN Salary bigint;
+
+update employee_payroll 
+set Gender ='M' where Name='Suresh';
+
+select sum(Salary) from employee_payroll 
+where Gender='M'Group By Gender;
+
+Select Avg(Salary) from employee_payroll
+where Gender='M' Group by Gender;
+
+select min(Salary) from employee_payroll
+where Gender='F' group by Gender;
+
+select count(Salary) from employee_payroll
+where Gender='M' group by Gender;
